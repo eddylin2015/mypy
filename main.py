466e42eb1cc03db88b318e8034d4dfc483a121cc
lin wu <eddylin2015@gmail.com>
2017-12-18 09:07:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,21 @@ import time
 def main():
     print(sys.argv)
     host="127.0.0.1"
-    path="/login"
-    params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+    path="/auth/login"
+    params = urllib.parse.urlencode({'@u': 12524, '@p': 'issue', '@action': 'show'})
     session_id=wg_utils.Auth_Login(host,path,params)
-    print(session_id)
     link_paths = [
   '/',
   '/control/device',
   '/control/devices',
   '/log/log_groups',
-  '/dashboard/executive?sn=2',
-  '/report/get_daily_reports?sn=2'
+  '/log/log_vpns',
+  '/log/log_devices?_search=false&rows=10000&page=1&sidx=name&sord=asc',
+  '/log/log_grid_columns?l_t=servers',
     ];
     for link_ in link_paths:
         wg_utils.HttpGet(host,link_, session_id)
         time.sleep(3)
-
 # Standard boilerplate to call the main() function to begin
 # the program.
 if __name__ == '__main__':
