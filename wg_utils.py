f61446ca4f73594898b5e7cbd9889869604bda08
lin wu <eddylin2015@gmail.com>
2017-12-18 12:33:25 +0800

    Update wg_utils.py

--- wg_utils.py
+++ wg_utils.py
@@ -2,8 +2,6 @@ import sys
 import http.client
 import urllib
 import ssl
-
-#Login Web Site: return Session-id
 def Auth_Login(host, path, param):
    ssl._create_default_https_context=ssl._create_unverified_context
    headers = {
@@ -18,7 +16,7 @@ def Auth_Login(host, path, param):
       'Accept-Encoding': 'gzip, deflate, br',
       'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4',
       'Cookie': 'session_id=714b232ee2cbdc32bf66ffff226b7b4388026a8c'
-    }
+   }
    conn = http.client.HTTPSConnection(host)
    conn.request("POST",path, param, headers)
    response = conn.getresponse()
@@ -30,8 +28,8 @@ def Auth_Login(host, path, param):
    print(data)
    conn.close()
    return session_id
-#end auto_login
-#HttpGet with Session_id
+   #end auto_login
+   #HttpGet with Session_id
 def HttpGet(host_, path_ , session_id):
    ssl._create_default_https_context=ssl._create_unverified_context
    headers ={ 'Cookie': session_id }
@@ -45,3 +43,56 @@ def HttpGet(host_, path_ , session_id):
    print(data)
    conn.close()
 #HttpGet End
+
+class WG:
+    #Common base class for all Install
+    wg_login_count=0
+    def __init__(self, host_):
+      self.session_id="none"
+      print(host_)
+      self.hostname=host_
+      WG.wg_login_count=WG.wg_login_count+1
+    def Auth_Login(host, path, param):
+      ssl._create_default_https_context=ssl._create_unverified_context
+      headers = {
+      'Connection': 'keep-alive',
+      'Content-Length': len(param),
+      'Cache-Control': 'max-age=0',
+      'Origin': 'https://' + host,
+      'Upgrade-Insecure-Requests': 1,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4',
+      'Cookie': 'session_id=714b232ee2cbdc32bf66ffff226b7b4388026a8c'
+      }
+      conn = http.client.HTTPSConnection(host)
+      conn.request("POST",path, param, headers)
+      response = conn.getresponse()
+      print( response.status, response.reason)
+      print(response.headers)
+      self.session_id=response.headers['set-cookie']
+      print(session_id)
+      data = response.read()
+      print(data)
+      conn.close()
+      return self.session_id
+    def Auth_Login(path, param):
+      return self.Auth_Login(self.hostname,path,param)
+    def HttpGet(path_):
+      self.HttpGet(self.host,path_,self.session_id) 		
+    def HttpGet(host_, path_ , session_id):
+      ssl._create_default_https_context=ssl._create_unverified_context
+      headers ={ 'Cookie': session_id }
+      conn = http.client.HTTPSConnection(host_)
+      body=""
+      conn.request("GET",str( path_), body, headers)
+      response = conn.getresponse()
+      print(response.status, response.reason)
+      print(response.headers)
+      data = response.read()
+      print(data)
+      conn.close()
+   #HttpGet End
+
